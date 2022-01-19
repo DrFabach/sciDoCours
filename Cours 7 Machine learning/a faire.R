65e33d481243639b1497e9d6efe265c9cc326940
DrFabach <55923151+DrFabach@users.noreply.github.com>
2022-01-19 12:32:55 +0100

    cours7

---
+++ Cours 7 Machine learning/a faire.R
@@ -0,0 +1,7 @@
+# A faire
+
+# Entrainer un algorithme (lm, glm ou knn) pour predire hospital_los_day ou hosp_exp_flg.
+
+# Utiliser une méthode d'apprentissage (cross validation, validation simple,
+# validation simple avec remise...) pour selectionner le meilleur modèle et ces hyperparamètres.
+
